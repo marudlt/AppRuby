9d2d8e5bad4044b4c49f462da8bc39e86cc57e5b
arinatale <51122614+arinatale@users.noreply.github.com>
2019-06-24 17:53:07 -0300

    Arreglo de objeto null

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+
+    def index
+        @tutor='El grupo de Ruby On Rails'
+    
+        @cursoscargados=Curso.count
+        @aulascargadas=Aula.count
+    
+        @aulastodas=Aula.all
+        @cursostodos=Curso.all
+    
+    
+       
+    
+    @cursosmañana=Curso.where(turno:'Mañana')
+    @cursostarde=Curso.where(turno:'Tarde')
+    @cursosnoche=Curso.where(turno:'Noche')
+    
+    @dias=["Lunes","Martes","Miecoles","Jueves","Viernes"]
+    
+      end
+
+      
 end
